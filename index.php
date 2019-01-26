1c4595e2396f5b36bb1755939690092062540995
Danijel Šugar <danijel.sugar@outlook.com>
2019-01-26 20:10:08 +0100

    Switch to choose what happens based on user input, function to show statistic menu

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 while( true ) { //infinite while loop to keep showing menu
 
     // Print the menu on console
-    printMenu();
+    if(!isset($choice)){
+        printMenu();
+    }
+
 
     // Read user choice
     $choice = trim( fgets(STDIN) );
@@ -12,18 +15,46 @@ while( true ) { //infinite while loop to keep showing menu
         break;
     }
 
+    switch ($choice) {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+            statisticMenu();
+            break;
+    }
+
 }
 
 function printMenu()
 {
-    echo "************ Reservation System ******************\n";
+    echo "************ Menu ******************\n";
     echo "1 - Pregled Zaposlenika\n";
     echo "2 - Unos novog Zaposlenika\n";
     echo "3 - Promjena podataka postojećem zaposleniku\n";
     echo "4 - Brisanje Zaposlenika\n";
     echo "5 - Statistika\n";
     echo "6 - Quit\n";
-    echo "************ Reservation System ******************\n";
-    echo "Enter your choice from 1 to 6 ::";
+    echo "************ Menu ******************\n";
+    echo "Odaberite broj od 1 do 6 ::";
+}
+
+function statisticMenu()
+{
+    echo "************ Statistika ******************\n";
+    echo "1 - Ukupna starost\n";
+    echo "2 - Prosječna starost\n";
+    echo "3 - Ukupna primanja\n";
+    echo "4 - Prosječna primanja\n";
+    echo "Odaberite broj od 1 do 4 ::";
 }
 
