bc9444437fa5f1b1a802687421713622488df6fe
Danijel Šugar <danijel.sugar@outlook.com>
2019-01-27 16:00:19 +0100

    Experimenting with adding new user

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
+
+require 'Objects/Person.php';
+
 while( true ) { //infinite while loop to keep showing menu
 
     // Print the menu on console
-    if(!isset($choice)){
+    if(!isset($choice)) {
         printMenu();
     }
-
 
     // Read user choice
     $choice = trim( fgets(STDIN) );
@@ -17,20 +19,38 @@ while( true ) { //infinite while loop to keep showing menu
 
     switch ($choice) {
         case 1:
-
+            echo "Pregled zaposlenika\n";
             break;
         case 2:
-
+            echo "Unos zaposlenika:\n";
+            echo "Ime: ";
+            $firstName = fgets(STDIN);
+            echo $firstName;
+            echo "Prezime: ";
+            $lastName = fgets(STDIN);
+            echo $lastName;
+            echo "Datum rođenja: ";
+            $birthDate = fgets(STDIN);
+            echo $birthDate;
+            echo "Spol: ";
+            $gender = fgets(STDIN);
+            echo $gender;
+            $person = new Person($firstName,$lastName,$birthDate,$gender);
+            echo $person->getId() . ' ' . $person->getFirstName() . ' ' . $person->getLastName() . ' ' .
+                $person->getBirthDate() . ' ' . $person->getGender();
+            print_r($person);
             break;
         case 3:
-
+            echo "Promjena";
             break;
         case 4:
-
+            echo "Brisanje";
             break;
         case 5:
             statisticMenu();
             break;
+        default:
+            echo "Navedite valjani izbor\n";
     }
 
 }
@@ -43,18 +63,18 @@ function printMenu()
     echo "3 - Promjena podataka postojećem zaposleniku\n";
     echo "4 - Brisanje Zaposlenika\n";
     echo "5 - Statistika\n";
-    echo "6 - Quit\n";
+    echo "6 - Izlaz\n";
     echo "************ Menu ******************\n";
-    echo "Odaberite broj od 1 do 6 ::";
+    echo "Odaberite broj: ";
 }
 
 function statisticMenu()
 {
     echo "************ Statistika ******************\n";
-    echo "1 - Ukupna starost\n";
-    echo "2 - Prosječna starost\n";
-    echo "3 - Ukupna primanja\n";
-    echo "4 - Prosječna primanja\n";
-    echo "Odaberite broj od 1 do 4 ::";
+    echo "7 - Ukupna starost\n";
+    echo "8 - Prosječna starost\n";
+    echo "9 - Ukupna primanja\n";
+    echo "10 - Prosječna primanja\n";
+    echo "Odaberite broj: ";
 }
 
