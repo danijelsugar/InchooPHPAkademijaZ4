c0cddc49632a48c936de96563467e192566d68ed
Danijel Šugar <danijel.sugar@outlook.com>
2019-01-27 17:41:13 +0100

    Class Employee extends properties and methods of Person, income property for employees only

--- Objects/Employee.php
+++ Objects/Employee.php
@@ -1,1 +1,25 @@
 <?php
+
+require 'Person.php';
+
+class Employee extends Person
+{
+    private $income;
+
+    public function __construct($firstName, $lastName, $birthDate, $gender, $income)
+    {
+        parent::__construct($firstName, $lastName, $birthDate, $gender);
+        $this->income = $income;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIncome()
+    {
+        return $this->income;
+    }
+}
+
+//$employee = new Employee('Danijel','Sugar','30.04.1997', 'M',99.99);
+//echo $employee->getFirstName() . ' ' . $employee->getLastName() . ' ' . $employee->getIncome();
