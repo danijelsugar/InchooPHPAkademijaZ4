568a71d354e8722569ea52876d886e646d0a00b8
Danijel Šugar <danijel.sugar@outlook.com>
2019-01-28 14:27:24 +0100

    New approach, using only employee object

--- Objects/Employee.php
+++ Objects/Employee.php
@@ -1,14 +1,97 @@
 <?php
 
-require 'Person.php';
-
-class Employee extends Person
+class Person
 {
-    private $income;
+    protected static $instance = 0;
+    protected $id = null;
+    protected $firstName;
+    protected $lastName;
+    protected $birthDate;
+    protected $gender;
+    protected $income;
 
-    public function __construct($firstName, $lastName, $birthDate, $gender, $income)
+    public function __construct()
     {
-        parent::__construct($firstName, $lastName, $birthDate, $gender);
+        $this->id = ++self::$instance;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $birthDate
+     */
+    public function setBirthDate($birthDate)
+    {
+        $this->birthDate = $birthDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBirthDate()
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $income
+     */
+    public function setIncome($income)
+    {
         $this->income = $income;
     }
 
@@ -21,5 +104,9 @@ class Employee extends Person
     }
 }
 
-//$employee = new Employee('Danijel','Sugar','30.04.1997', 'M',99.99);
-//echo $employee->getFirstName() . ' ' . $employee->getLastName() . ' ' . $employee->getIncome();
+//$person = new Person(1,'Danijel', 'Sugar','30.04.1997','Musko');
+//$person->setIme = 'Pero';
+//$person->setPrezime = 'Perić';
+
+
+//echo $person->getFirstName() . ' ' . $person->getLastName() . ' ' . $person->getBirthDate();
